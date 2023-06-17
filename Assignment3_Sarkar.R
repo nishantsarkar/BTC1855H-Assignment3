7c5536a933dd3d57ff8b6ff3cb2457ccd110c4b6
Nishant <nsrk01@gmail.comm>
2023-06-17 18:03:27 -0400

    Random word sampling

--- Assignment3_Sarkar.R
+++ Assignment3_Sarkar.R
@@ -1,2 +1,8 @@
 #' Assignment 3
 #' This script allows the user to play Hangman in R
+
+#' Loading words from a text file into a vector, then randomly sampling one word
+#' for the game. Please ensure that words.txt is in your working directory. 
+wordsList <- readLines("words.txt")
+secretWord <- sample(wordsList, 1)
+
