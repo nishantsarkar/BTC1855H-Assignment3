e1d1c7121ccc2ca3324e4802b9a318ed5b3f927b
Nishant <nsrk01@gmail.comm>
2023-06-21 19:05:24 -0400

    Game intro, secretWord length

--- Assignment3_Sarkar.R
+++ Assignment3_Sarkar.R
@@ -1,7 +1,20 @@
 #' Assignment 3
 #' This script allows the user to play Hangman in R
+#' Please ensure that words.txt is in your working directory. 
 
 #' Loading words from a predefined list of words into the character vector 
 #' wordsList using readLines(). One word is randomly sampled using sample(). 
 wordsList <- readLines("words.txt")
 secretWord <- sample(wordsList, 1)
+
+#' Introduction to the game given to the player. nchar() is used to provide
+#' the player with the number of characters in the secret word. Each text line
+#' is separated by a line using /n to improve text readability. 
+secretWordLength <- nchar(secretWord)
+
+cat("Welcome to Medical Device Hangman!\n")
+cat("Your goal is to guess the secret word letter-by-letter.\n")
+cat("If you want, you can also guess the whole word.\n")
+cat("Be careful, if you guess wrong six times, you get hanged!\n")
+cat("Your secret word has", secretWordLength, "letters.\n")
+
