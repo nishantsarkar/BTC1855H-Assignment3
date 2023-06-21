5f68fa52f8b9c2533d9e5b8e800e303b5cbb3747
Nishant <nsrk01@gmail.comm>
2023-06-21 19:13:30 -0400

    reorganized variable initialization

--- Assignment3_Sarkar.R
+++ Assignment3_Sarkar.R
@@ -7,11 +7,21 @@
 wordsList <- readLines("words.txt")
 secretWord <- sample(wordsList, 1)
 
-#' Introduction to the game given to the player. nchar() is used to provide
-#' the player with the number of characters in the secret word. Each text line
-#' is separated by a line using /n to improve text readability. 
+#' Initializing variables for future use.
+wrongGuessesMax <- 6
+wrongGuessesCurrent <- 0
+
+#' nchar() is used to provide the player with the number of characters in 
+#' the secret word.
 secretWordLength <- nchar(secretWord)
 
+#' The correctLetters vector is initialized to contain the same number of
+#' underscores as the letters in secretWord, indicating that no letters have
+#' been successfully guessed yet. This is updated in the main gameplay loop.
+correctLetters <- rep("_", secretWordLength)
+
+#' Introduction to the game given to the player. Each text line
+#' is separated by a line using /n to improve text readability. 
 cat("Welcome to Medical Device Hangman!\n")
 cat("Your goal is to guess the secret word letter-by-letter.\n")
 cat("If you want, you can also guess the whole word.\n")
