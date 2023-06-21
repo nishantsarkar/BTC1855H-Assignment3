bbeb08b6ac3bcc960520b34f236082248b784f11
Nishant <nsrk01@gmail.comm>
2023-06-21 18:57:44 -0400

    Re-initial commit; changing approach

--- Assignment3_Sarkar.R
+++ Assignment3_Sarkar.R
@@ -1,35 +1,7 @@
 #' Assignment 3
 #' This script allows the user to play Hangman in R
 
-# This helper function initializes all the variables in the game.
-initialization <- function() {
-  #' Loading words from a text file into a vector, then randomly sampling one word
-  #' for the game. Please ensure that words.txt is in your working directory. 
-  wordsList <- readLines("words.txt")
-  secretWord <- sample(wordsList, 1)
-  
-  #' The following vectors and counter are initialized here with empty vectors 
-  #' to eventually hold letters that have been guessed overall, letters which 
-  #' match those in secretWord, and the number of incorrect guesses. 
-  lettersGuessed <- vector()
-  lettersCorrect <- vector()
-  incorrectGuesses <- 0
-  
-  #' The initialization function compiles all the above into a list, which 
-  #' contains all information about the state of the game.
-  return(list(secretWord = secretWord,
-              lettersGuessed = lettersGuessed,
-              lettersCorrect = lettersCorrect,
-              incorrectGuesses = incorrectGuesses))
-} #' End of initialization() function block. This function is called in the
-  #' main gameplay loop, with the output list assigned to the variable gameState.
-
-# 
-
-# This function contains the main hangman gameplay. 
-hangman <- function() {
-  gameState <- initialization()
-
-} # End of hangman() function block
-
-
+#' Loading words from a predefined list of words into the character vector 
+#' wordsList using readLines(). One word is randomly sampled using sample(). 
+wordsList <- readLines("words.txt")
+secretWord <- sample(wordsList, 1)
